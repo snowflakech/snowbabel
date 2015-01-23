aa671b0f210fe81356dd7b0d209015a8799c6fd6
Daniel Alder <dalder@snowflake.ch>
2015-01-23 17:23:52 +0100

    [TASK] Remove old code

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,16 +3,3 @@
 if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerExtDirectComponent(
-	'TYPO3.Snowbabel.ExtDirect',
-	'Snowflake\\Snowbabel\\Connection\ExtDirectServer'
-);
-
-// Add Scheduler Configuration For Indexing
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['Snowflake\\Snowbabel\\Task\\Indexing'] = array (
-	'extension' => $_EXTKEY,
-	'title' => 'Snowbabel - Indexing',
-	'description' => 'Indexes all translation on current installation',
-	'additionalFields' => '',
-);
