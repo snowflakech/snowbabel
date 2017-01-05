600385de8314edce935696dd11bd4aae214236eb
ricky.mk <ricky.mk@pitsolutions.com>
2017-01-05 12:22:45 +0530

    update in version

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.4-7.0.99',
-			'typo3' => '6.2.0-8.4.99',
+			'typo3' => '6.2.0-8.5.99',
 			'static_info_tables' => '6.1.0-6.3.99',
 		),
 		'conflicts' => array(),
@@ -24,5 +24,5 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Daniel Alder, Ricky Mathew',
 	'author_email' => 'ricky.mk@pitsolutions.com',
 	'author_company' => 'PIT Solutions Pvt Ltd',
-	'version' => '5.0.0'
+	'version' => '5.0.1'
 );
